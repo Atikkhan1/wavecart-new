6716f4cbe60ffe78bc3a5d002cb8ed04ddf8c3e0
Atikkhan1 <khanarbajpathan057@gmail.com>
2024-07-12 22:54:47 +0530

    product page

--- app/product/[ProductId]/page.jsx
+++ app/product/[ProductId]/page.jsx
@@ -29,69 +29,38 @@ export default function ProductPage ({params}) {
   
     
   return (
-    <section className="text-gray-600 body-font overflow-hidden">
-    <div className="container px-5 py-24 mx-auto">
+<div>
+<div className="flex w-full bg-slate-200 h-20 absolute bottom-0 md:justify-end">
+  <button className="flex w-1/3 max-md:m-auto my-auto h-2/3 md:w-1/6  md:mx-4 text-gray-600 border-2 border-current py-2 px-6 focus:outline-none hover:bg-gray-300 rounded justify-center items-center">
+    Add to Cart
+  </button>
+  <button className="flex w-1/3 max-md:m-auto my-auto h-2/3 md:w-1/6  md:mx-4 text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-300 rounded justify-center items-center">
+    Buy Now
+  </button>
+
+</div>
+<section className="text-gray-600 body-font overflow-hidden">
+    <div className="container px-5 pt-6 mx-auto">
       <div className="lg:w-4/5 mx-auto flex flex-wrap">
         <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-auto object-cover object-center rounded" src={Data.image}/>
               
         <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-          <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-          <h1 className="text-black text-3xl title-font font-medium mb-1">{Data.name}</h1>
-         
+          <h2 className="text-xl title-font text-gray-500 tracking-widest">{Data.category}</h2>
+          <h1 className="text-black text-2xl title-font font-medium mb-1">{Data.name}</h1>
+          
           <div className="flex">
-            <p className="title-font font-medium text-2xl text-green-400">₹{Math.floor(Data.price * 1.5)}/- <b className='ml-2 text-gray-400 line-through'>₹{Math.floor(Data.price * 1.7)}</b></p>
-            <button className="flex ml-8 text-white bg-pink-800 border-0 py-2 px-6 focus:outline-none hover:bg-pink-500 rounded">Buy Now</button>
-            <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-5">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-              </svg>
-            </button>
+            <p className="title-font font-medium text-3xl my-2 text-green-400">₹ {Math.floor(Data.price * 1.5)} /- 
+              <b className='ml-2 text-xl text-gray-400 line-through'>₹ {Math.floor(Data.price * 1.7)}/-</b> 
+              <b className="ml-5 italic text-2xl">{Math.floor((Data.price * 1.7 - Data.price * 1.5)/(Data.price * 1.7)*100)}% off</b></p>
           </div>
-          <div className="flex mb-4 mt-2">
-            <span className="flex items-center">
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-              <span className="text-gray-600 ml-3">4 Reviews</span>
-            </span>
-            <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-              <a className="text-gray-500">
-                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a className="text-gray-500">
-                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a className="text-gray-500">
-                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-          
-          <p className="leading-relaxed mt-2" dangerouslySetInnerHTML={{ __html: Data.description }}></p>
 
-          
+          <p className="leading-relaxed mt-2 overflow-y-scroll h-48 border-2 p-4" dangerouslySetInnerHTML={{ __html: Data.description }}></p>
         </div>
       </div>
     </div>
   </section>
+
+</div>
   )
 }
 
