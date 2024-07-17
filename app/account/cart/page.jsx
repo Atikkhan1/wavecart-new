e773c482554db4f83a19346959288d3669a94a4a
Atikkhan1 <khanarbajpathan057@gmail.com>
2024-07-17 17:30:49 +0530

    cart css

--- app/account/cart/page.jsx
+++ app/account/cart/page.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from "react";
 import { useLocalStorage } from "../../useLocalStorage";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import Loading from "../../loading";
 
 export default function page() {
   let storage = useLocalStorage("_id");
   let router = useRouter();
   const [product, setProduct] = useState([]);
+  const [loader, setloader] = useState(<Loading></Loading>);
   let id = storage.getItem();
 
   useEffect(() => {
@@ -23,10 +25,14 @@ export default function page() {
           }),
         });
         const jsonData = await response.json();
+        
         let inCartArray = jsonData.productData.inCart;
+        if(inCartArray==''){
+          setloader(<div className="flex justify-center text-3xl italic text-gray-400">Oh! No product in your Cart</div>)
+        }
         const ele = [];
         for (let i = 0; i < inCartArray.length; i++) {
-          const res = await fetch(`/api/getProducts`, {
+          await fetch(`/api/getProducts`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -35,19 +41,26 @@ export default function page() {
               key: "_id",
               value: inCartArray[i],
             }),
-          });
+          }).then(async(res)=>{
           const products = await res.json();
           ele.push(products[0]);
+          console.log(ele)
           setProduct(ele);
+          setloader('')})
         }
       } else {
         router.push("/account/login");
       }
+      
+        
+      
     };
     fetchProduct();
   }, []);
+
+
   const removeToCart = async(slug) =>{
-    if (storage.getItem()!=undefined){
+      const ele = [];
       const response = await fetch(`/api/account/cart/remove`, {
         method: 'POST',
         headers: {
@@ -57,18 +70,16 @@ export default function page() {
           accountId:storage.getItem(),
           productId:slug
         }),
-      });
-      const jsonData = await response.json();
-      router.push('/account')
-    }else{
-      router.push("/auth/login")
+      }).then(()=>{
+        router.refresh()
+      })
     }
-  }
+  
     
   const Cartdesign = (param) => {
     let p = param.who;
     return (
-      <div className="flex w-full h-32 border p-2">
+      <div className="flex w-full md:w-5/12 h-32  p-2">
         <img src={p.image} alt="" className="h-full w-24 border" />
         <div className="block h-full w-2/3 ml-auto ">
           <p className=" flex h-6 w-full  overflow-y-clip ">
@@ -77,7 +88,7 @@ export default function page() {
           <p className="title-font font-medium text-xl my-2 text-green-400">₹ {Math.floor(p.price * 1.5)} /- 
               <b className='ml-2 text-xl text-gray-400 line-through'>₹ {Math.floor(p.price * 1.7)}/-</b> 
               </p>
-          <div className="flex mt-auto  gap-2">
+          <div className="flex mt-auto justify-end gap-2">
             <button onClick={()=>{removeToCart(p._id)}} className="flex p-2 h-10 w-24 justify-center rounded-md hover:translate-y-1 bg-red-500 text-white">
               Remove
             </button>
@@ -91,7 +102,8 @@ export default function page() {
   };
   return (
     <div className="bg-slate-100  w-full">
-      <div className="w-full bg-white mt-6 ">
+      <div className="md:flex gap-x-20 w-full bg-white mt-6 ">
+        <div className="mb-4">{loader}</div>
         {product.map((p)=>{
             return(
               <Cartdesign key={p._id} who={p}/>
