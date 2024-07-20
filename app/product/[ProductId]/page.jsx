5bbb449b488f46cd80f02d47cd9896c0a0ede603
Atikkhan1 <khanarbajpathan057@gmail.com>
2024-07-20 14:24:56 +0530

    w

--- app/product/[ProductId]/page.jsx
+++ app/product/[ProductId]/page.jsx
@@ -52,7 +52,7 @@ export default function ProductPage ({params}) {
   return (
 <div className="">
 <div className="flex w-full bg-slate-200 h-20 absolute bottom-0 md:justify-end">
-  <button onClick={()=>{addToCart()}} className="flex w-1/3 max-md:m-auto my-auto h-2/3 md:w-1/6  md:mx-4 text-gray-600 border-2 border-current py-2 px-6 focus:outline-none hover:bg-gray-300 rounded justify-center items-center">
+  <button onClick={()=>{addToCart()}} className="flex w-2/5 max-md:m-auto my-auto h-2/3 md:w-1/6  md:mx-4 text-gray-600 border-2 border-current py-2 px-6 focus:outline-none hover:bg-gray-300 rounded justify-center items-center">
     Add to Cart
   </button>
   <button className="flex w-1/3 max-md:m-auto my-auto h-2/3 md:w-1/6  md:mx-4 text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-300 rounded justify-center items-center">
