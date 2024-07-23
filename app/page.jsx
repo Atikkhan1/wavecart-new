905fe43b7c5124230f46460c29b084c5b031925d
Atikkhan1 <khanarbajpathan057@gmail.com>
2024-07-23 20:44:36 +0530

    aabra ka dabra

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import ProductDesign from "./components/ProductDesign";
 import Categorybar from "./components/Categorybar";
 import Loading from "./loading";
 import Head from "next/head";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -28,18 +29,26 @@ export default function Home() {
       {loader}
       <div className={loader == ""?"":"hidden"}>
         
-      <Categorybar />
+      <Categorybar /> 
 
-
-      <div className="flex overflow-x-scroll items-center  w-full "> 
-      <img className="w-full object-fit flex-1 p-2" src="https://static.vecteezy.com/system/resources/previews/002/006/775/non_2x/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-backgroud-for-banner-market-ecommerce-free-vector.jpg" alt="" />
-      <img className="w-full object-fit flex-1 p-2" src="/watch-banner.png" alt="" />
-      <img className="w-full object-fit flex-1 p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa5o_cG6Hag6U5UPWR4SK5BMlZCNNc9IXy0Q&s" alt="" />
-      <img className="w-full object-fit flex-1 p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdHSbwaFDloPrGn-VqXFHqDJvpeoiAe69_ZQ&s" alt="" />
-      <img className="w-full object-fit flex-1 p-2" src="" alt="" />
-      <img className="w-full object-fit flex-1 p-2" src="" alt="" />
+      <div className="flex w-full h-10 max-md:overflow-hidden items-center bg-green-300  p-1 ">
+        <span className="flex scroll-element italic whitespace-nowrap">❤️ Sale is going on !!! , what are you waiting for ? Buy at lowest price Now! 👍</span> 
       </div>
 
+
+      <div className="flex h-40 w-full mx-0 my-auto overflow-x-scroll scroll-smooth "> 
+        <img id="page-1" className="block justify-center items-center w-full h-full p-2" src="https://static.vecteezy.com/system/resources/previews/002/006/775/non_2x/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-backgroud-for-banner-market-ecommerce-free-vector.jpg" alt="" />
+        <img id="page-2" className="block justify-center items-center w-full h-full p-2" src="/watch-banner.png" alt="" />
+        <img id="page-3" className="block justify-center items-center w-full h-full p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa5o_cG6Hag6U5UPWR4SK5BMlZCNNc9IXy0Q&s" alt="" />
+        <img id="page-4" className="block justify-center items-center w-full h-full p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdHSbwaFDloPrGn-VqXFHqDJvpeoiAe69_ZQ&s" alt="" />
+      {/* <img className="flex justify-center items-center w-full m-2" src="" alt="" /> */}
+      </div>
+        <div className="flex justify-center my-2 w-full h-auto  ">
+          <button onClick={(e)=>"page-1"} className="border mx-2 my-auto w-3 h-3 rounded-full border-gray-500 hover:bg-gray-500" ></button>
+          <button onClick={(e)=>"page-2"} className="border mx-2 my-auto w-3 h-3 rounded-full border-gray-500 hover:bg-gray-500" ></button>
+          <button onClick={(e)=>"page-3"} className="border mx-2 my-auto w-3 h-3 rounded-full border-gray-500 hover:bg-gray-500" ></button>
+          <button onClick={(e)=>"page-4"} className="border mx-2 my-auto w-3 h-3 rounded-full border-gray-500 hover:bg-gray-500" ></button>
+        </div>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto">
           <div className="flex flex-wrap max-md:gap-x m-4">
